7c79694eaa654696b5332551663ad52164281372
ana.su._ <98901363+ANASUDDEEN-PP@users.noreply.github.com>
2025-03-06 14:43:18 +0530

    Delete server directory

--- server/Routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../Controllers/authController');
-
-router.post('/login', authController.login);
-router.post('/verify', authController.verifyCodre);
-
-module.exports = router;
